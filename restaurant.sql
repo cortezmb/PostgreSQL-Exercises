28619331b6aa05548a5a7087934fee45b4430c49
cortezmb <michael.cortez78@gmail.com>
2020-07-23 18:41:53 -0500

    added data to table

--- restaurant.sql
+++ restaurant.sql
@@ -1,10 +1,18 @@
 
-CREATE TABLE restaurant (
-    name varchar,
-    distance_in_miles_from_HeadQuarters integer,
-    stars integer,
-    category_type_of_food varchar,
-    favorite_dish varchar,
-    does_takeout boolean,
-    last_time_you_ate_there date
-);
+-- CREATE TABLE restaurant (
+--     name varchar,
+--     distance_in_miles_from_HeadQuarters integer,
+--     stars integer,
+--     category_type_of_food varchar,
+--     favorite_dish varchar,
+--     does_takeout boolean,
+--     last_time_you_ate_there date
+-- );
+
+INSERT INTO restaurant VALUES
+('Zoes Kitchen', 3, 3, 'Greek', 'lamb kabobs', TRUE, '2020-03-01'),
+('Main Chick', 2, 5, 'American Sandwiches', 'slider', TRUE, '2020-02-21'),
+('The Pit Room', 3, 4, 'Barbecue', 'pork ribs', TRUE, '2020-02-24'),
+('Island 1515', 6, 5, 'Caribbean', 'jerk chicken', TRUE, '2019-09-05'),
+('Turkey Leg Hut', 2, 5, 'Southern Cajun/Creole', 'sweet hennessy glazed turkey leg', TRUE, '2019-11-10'),
+('Ritual', 1, 5, 'Southern American Steakhouse', 'tomahawk ribeye steak', TRUE, '2019-12-18');
