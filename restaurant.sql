74e886fb789ac5c3ff3529329613da1d8218f4df
cortezmb <michael.cortez78@gmail.com>
2020-07-23 15:51:00 -0500

    created table named restaurant

---
+++ restaurant.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE restaurant (
+    name varchar,
+    distance_in_miles_from_HeadQuarters integer,
+    stars integer,
+    category_type_of_food varchar,
+    favorite_dish varchar,
+    does_takeout boolean,
+    last_time_you_ate_there date
+);
