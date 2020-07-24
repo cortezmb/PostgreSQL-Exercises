f9b6092277cee05a7dc1fd8c9de2613d425d4eb0
cortezmb <michael.cortez78@gmail.com>
2020-07-24 06:11:23 -0500

    completed first 2 exercises

--- restaurant.sql
+++ restaurant.sql
@@ -9,10 +9,19 @@
 --     last_time_you_ate_there date
 -- );
 
-INSERT INTO restaurant VALUES
-('Zoes Kitchen', 3, 3, 'Greek', 'lamb kabobs', TRUE, '2020-03-01'),
-('Main Chick', 2, 5, 'American Sandwiches', 'slider', TRUE, '2020-02-21'),
-('The Pit Room', 3, 4, 'Barbecue', 'pork ribs', TRUE, '2020-02-24'),
-('Island 1515', 6, 5, 'Caribbean', 'jerk chicken', TRUE, '2019-09-05'),
-('Turkey Leg Hut', 2, 5, 'Southern Cajun/Creole', 'sweet hennessy glazed turkey leg', TRUE, '2019-11-10'),
-('Ritual', 1, 5, 'Southern American Steakhouse', 'tomahawk ribeye steak', TRUE, '2019-12-18');
+-- INSERT INTO restaurant VALUES
+-- ('Zoes Kitchen', 3, 3, 'Greek', 'lamb kabobs', TRUE, '2020-03-01'),
+-- ('Main Chick', 2, 5, 'American Sandwiches', 'slider', TRUE, '2020-02-21'),
+-- ('The Pit Room', 3, 4, 'Barbecue', 'pork ribs', TRUE, '2020-02-24'),
+-- ('Island 1515', 6, 5, 'Caribbean', 'jerk chicken', TRUE, '2019-09-05'),
+-- ('Turkey Leg Hut', 2, 5, 'Southern Cajun/Creole', 'sweet hennessy glazed turkey leg', TRUE, '2019-11-10'),
+-- ('Ritual', 1, 5, 'Southern American Steakhouse', 'tomahawk ribeye steak', TRUE, '2019-12-18');
+
+--The names of the restaurants that you gave a 5 stars to
+--  SELECT * FROM restaurant WHERE stars > 4;
+
+--The favorite dishes of all 5-star restaurants
+-- SELECT favorite_dish FROM restaurant WHERE stars = 5;
+
+--The the id of a restaurant by a specific restaurant name, say 'Moon Tower'
+SELECT * FROM restaurant WHERE stars > 4;
